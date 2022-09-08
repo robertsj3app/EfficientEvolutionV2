b5f9dc7b239dd87927c02b59182a0eb8ba518b89
howardjw2 <jhoward405@gmail.com>
2022-09-08 15:31:45 -0400

    Added get_ids() method

--- tile.py
+++ tile.py
@@ -1,30 +1,39 @@
 class Tile:
     def __init__(self):
-        self._food = 0
-        self._temperature = 0
-        self._water = 0
-        self._individuals = []
+        self.food = 0
+        self.temperature = 0
+        self.water = 0
+        self.individuals = []
 
     def print(self):
-        print("OO")
+        print(len(self.individuals))
+
+    def insert_individual(self, ind):
+        self.individuals.append(ind)
 
     def get_food(self):
-        return self._food
+        return self.food
     def get_temperature(self):
-        return self._temperature
+        return self.temperature
     def get_water(self):
-        return self._water
+        return self.water
     def get_individuals(self):
-        return self._individuals
+        return self.individuals
+    def get_ids(self):
+        arr = []
+        for ind in self.individuals:
+            arr.append(ind.get_id())
+        return arr
 
     def set_food(self, inp):
-        self._food = inp
+        self.food = inp
     def set_temperature(self, inp):
-        self._temperature = inp
+        self.temperature = inp
     def set_water(self, inp):
-        self._water = inp
-    def set_individual(self, inp):
-        self._individuals = inp
+        self.water = inp
+    def set_individuals(self, inp):
+        self.individuals = inp
+
 
 
 
